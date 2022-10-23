5fb0f79e8ce0b51df3ef1eaa7894e1cb2bcff688
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-23 10:43:38 +0100

    working on rest api

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express'); //Import Express
+// const Joi = require('joi'); //Import Joi
+const app = express(); //Create Express Application on the app variable
+app.use(express.json()); //used the json file
